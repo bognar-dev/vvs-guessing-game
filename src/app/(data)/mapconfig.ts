42609a9973633112c8becb6c1fdee0c09f930299
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-12-04 14:58:26 +0000

    Update map style and station point color

--- src/app/(data)/mapconfig.ts
+++ src/app/(data)/mapconfig.ts
@@ -2,14 +2,15 @@ import { CircleLayer } from 'react-map-gl';
 
 export const mapConfig = {
     accessKey: "pk.eyJ1IjoibmlraWJnbnIiLCJhIjoiY2xwbXY4eWY5MGR1MzJxcW5sNXNtZXpiMyJ9.2bowtyVE-DbmLOTzcgXhgQ",
-    style: "mapbox://styles/mapbox/streets-v9",
+    style: "mapbox://styles/nikibgnr/clpmvcg2n00xv01pkhggs0bqv",
 
     stationLayerStyle: {
         id: 'StationPoint',
         type: 'circle',
         paint: {
             'circle-radius': 3,
-            'circle-color': '#007cbf'
+            'circle-color': '#000000'
+            
         }
     } as CircleLayer,
     guessedStationLayerStyle: {
