72ba9ef66b0fae7994f2137f09e36b33971dc48b
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-12-11 13:27:38 +0000

    Fix casing of viewport variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 }
 
-export const ViewPort: Viewport = {
+export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
