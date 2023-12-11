17e67733c4bab9ff0bf315d4a65b6f8f95952c60
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-12-11 13:13:41 +0000

    Add Viewport to metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
@@ -8,6 +8,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'VVS Memory Game',
   description: 'Kennst du alle Station der VVS?',
+
+}
+
+export const ViewPort: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({
